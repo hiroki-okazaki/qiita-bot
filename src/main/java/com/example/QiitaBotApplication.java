a10c761ffdf7904cef9b6db0d877b5b0cd54b6a2
hiroki-okazaki <1031okazaki@gmail.com>
2019-10-09 17:36:46 +0900

    経営理念返答機能

--- src/main/java/com/example/QiitaBotApplication.java
+++ src/main/java/com/example/QiitaBotApplication.java
@@ -1,18 +1,15 @@
 package com.example;
 
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.service.LineService;
 import com.linecorp.bot.client.LineMessagingService;
-import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
@@ -20,6 +17,10 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @LineMessageHandler
 public class QiitaBotApplication {
 
+    // LINEサービス
+    @Autowired
+    private LineService lineService;
+	
     public static void main(String[] args) {
         SpringApplication.run(QiitaBotApplication.class, args);
     }
@@ -30,19 +31,25 @@ public class QiitaBotApplication {
 //        return new TextMessage(event.getMessage().getText());
 //    }
     
-    
-    @Autowired
-    private LineMessagingService lineMessagingService;
-
     @EventMapping
-    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
-      System.out.println("event: " + event);
-      final BotApiResponse apiResponse = lineMessagingService
-          .replyMessage(new ReplyMessage(event.getReplyToken(),
-                                         Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
-          .execute().body();
-      System.out.println("Sent messages: " + apiResponse);
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(lineService.createResponseMessage(event.getMessage().getText()));
     }
+    
+    
+//    @Autowired
+//    private LineMessagingService lineMessagingService;
+
+//    @EventMapping
+//    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+//      System.out.println("event: " + event);
+//      final BotApiResponse apiResponse = lineMessagingService
+//          .replyMessage(new ReplyMessage(event.getReplyToken(),
+//                                         Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
+//          .execute().body();
+//      System.out.println("Sent messages: " + apiResponse);
+//    }
 
     @EventMapping
     public void defaultMessageEvent(Event event) {
---
+++ src/main/java/com/example/service/LineService.java
@@ -0,0 +1,100 @@
+package com.example.service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * LINEサービス
+ * 
+ * @author s-tsuchida
+ */
+@Component
+public class LineService {
+	
+
+	// 返答メッセージ
+	private static final String PHILOSOPHY_MESSAGE = "ITに関わるすべての人たちを応援する楽楽パートナー";
+	private static final String CORE_VALUE_01_MESSAGE = "1.応えるお客様の期待に120%応えます。";
+	private static final String CORE_VALUE_02_MESSAGE = "2.育成する結果が出せる人財を育成します";
+	private static final String CORE_VALUE_03_MESSAGE = "3.改善する日々その活動を改善します。";
+	private static final String CORE_VALUE_04_MESSAGE = "4.偽らないステークホルダーに対して偽りません。";
+	private static final String CORE_VALUE_05_MESSAGE = "5.進化する変化の予兆を読み取り柔軟に進化します。";
+	private static final String CORE_VALUE_ALL_MESSAGE = CORE_VALUE_01_MESSAGE + "\r\n" + CORE_VALUE_02_MESSAGE + "\r\n"
+			+ CORE_VALUE_03_MESSAGE + "\r\n" + CORE_VALUE_04_MESSAGE + "\r\n" + CORE_VALUE_05_MESSAGE + "\r\n";
+
+	// メッセージ該当外メッセージ
+	private static final String OTHER_MESSAGE = "ようこそ!\r\nSpringLineBotへ!";
+
+	// メッセージMAP
+	private static final Map<String, String> MESSAGE_MAP = Collections.unmodifiableMap(new HashMap<String, String>() {
+		private static final long serialVersionUID = 1L;
+
+		{
+
+
+			// 経営理念
+			put("philosophy", PHILOSOPHY_MESSAGE);
+			put("PHILOSOPHY", PHILOSOPHY_MESSAGE);
+			put("けいえいりねん", PHILOSOPHY_MESSAGE);
+			put("経営理念", PHILOSOPHY_MESSAGE);
+
+			// コアバリュー1
+			put("corevalue1", CORE_VALUE_01_MESSAGE);
+			put("COREVALUE1", CORE_VALUE_01_MESSAGE);
+			put("こあばりゅー1", CORE_VALUE_01_MESSAGE);
+			put("コアバリュー1", CORE_VALUE_01_MESSAGE);
+
+			// コアバリュー2
+			put("corevalue2", CORE_VALUE_02_MESSAGE);
+			put("COREVALUE2", CORE_VALUE_02_MESSAGE);
+			put("こあばりゅー2", CORE_VALUE_02_MESSAGE);
+			put("コアバリュー2", CORE_VALUE_02_MESSAGE);
+
+			// コアバリュー3
+			put("corevalue3", CORE_VALUE_03_MESSAGE);
+			put("COREVALUE3", CORE_VALUE_03_MESSAGE);
+			put("こあばりゅー3", CORE_VALUE_03_MESSAGE);
+			put("コアバリュー3", CORE_VALUE_03_MESSAGE);
+
+			// コアバリュー4
+			put("corevalue4", CORE_VALUE_04_MESSAGE);
+			put("COREVALUE4", CORE_VALUE_04_MESSAGE);
+			put("こあばりゅー4", CORE_VALUE_04_MESSAGE);
+			put("コアバリュー4", CORE_VALUE_04_MESSAGE);
+
+			// コアバリュー5
+			put("corevalue5", CORE_VALUE_05_MESSAGE);
+			put("COREVALUE5", CORE_VALUE_05_MESSAGE);
+			put("こあばりゅー5", CORE_VALUE_05_MESSAGE);
+			put("コアバリュー5", CORE_VALUE_05_MESSAGE);
+
+			// コアバリューすべて
+			put("corevalue", CORE_VALUE_ALL_MESSAGE);
+			put("COREVALUE", CORE_VALUE_ALL_MESSAGE);
+			put("こあばりゅー", CORE_VALUE_ALL_MESSAGE);
+			put("コアバリュー", CORE_VALUE_ALL_MESSAGE);
+
+		}
+	});
+
+	/**
+	 * 送信されたLINEメッセージをもとに返答メッセージを返す
+	 * 
+	 * @param sendMessage 送信されたLINEメッセージ
+	 * @return 返答メッセージ
+	 */
+	public String createResponseMessage(String sendMessage) {
+
+		if (MESSAGE_MAP.containsKey(sendMessage)) {
+			// 送信されたLINEメッセージがMAPのキーに存在する場合
+			return MESSAGE_MAP.get(sendMessage);
+		} else {
+			// 送信されたLINEメッセージがMAPのキーに存在しない場合
+			return OTHER_MESSAGE;
+		}
+	}
+
+}
