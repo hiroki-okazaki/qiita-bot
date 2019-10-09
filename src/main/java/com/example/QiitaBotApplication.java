0f7c9ea8589525dd19715c90cb7993d11eab6ade
hiroki-okazaki <1031okazaki@gmail.com>
2019-10-09 17:07:46 +0900

    userid返答機能

--- src/main/java/com/example/QiitaBotApplication.java
+++ src/main/java/com/example/QiitaBotApplication.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.client.LineMessagingService;
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
@@ -18,11 +24,34 @@ public class QiitaBotApplication {
         SpringApplication.run(QiitaBotApplication.class, args);
     }
 
+//    @EventMapping
+//    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+//        System.out.println("event: " + event);
+//        return new TextMessage(event.getMessage().getText());
+//    }
+    
+    
+    @Autowired
+    private LineMessagingService lineMessagingService;
+
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText());
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+      System.out.println("event: " + event);
+      final BotApiResponse apiResponse = lineMessagingService
+          .replyMessage(new ReplyMessage(event.getReplyToken(),
+                                         Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
+          .execute().body();
+      System.out.println("Sent messages: " + apiResponse);
     }
+
+    @EventMapping
+    public void defaultMessageEvent(Event event) {
+      System.out.println("event: " + event);
+    }
+    
+    
+    
+    
     
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
