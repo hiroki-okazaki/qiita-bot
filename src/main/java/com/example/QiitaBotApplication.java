dda5bd2f97d4c9569277101e7e366b6eb733e420
hirokiokazaki <1031okazaki@gmail.com>
2019-10-23 08:58:33 +0900

    update

--- src/main/java/com/example/QiitaBotApplication.java
+++ src/main/java/com/example/QiitaBotApplication.java
@@ -50,7 +50,7 @@ public class QiitaBotApplication {
         return text;
         }
     
-    @Scheduled(cron = "0 58 8 * * *", zone = "Asia/Tokyo")
+    @Scheduled(cron = "0 0 18 * * *", zone = "Asia/Tokyo")
     public void doSomething() throws Exception {
     	pushConfirmController.pushMessage();
     }
